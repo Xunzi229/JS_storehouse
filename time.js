9922a967bcd1e03e6d95dfa1f6ddf3bb1a3940f6
xuzhi <xu.zhi@rongyu100.com>
2017-04-25 16:38:44 +0800

    Modifu time.js

--- time.js
+++ time.js
@@ -76,3 +76,24 @@ var Time = {
     var val = d.getFullYear()+"年"+month+"月"+day+"日";
     return val;
   }
+
+
+ //#yyyy-MM-dd 获取当前的时间
+  function current_time(){
+    var date = new Date();
+    var seperator = "-";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + "年" + month + "月" + strDate + "日";
+    // Hours = date.getHours()
+    // Minutes = date.getMinutes()
+    // getSeconds = date.getSeconds();
+    return currentdate;
+
+  }
